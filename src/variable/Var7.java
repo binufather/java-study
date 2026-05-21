2a6bd90fa6a18b0d83dc480ae926b1e7ef9def73
hanjiwon <gomdeschool@naver.com>
2026-05-21 20:58:05 +0900

    5.21 변수 및 변수 타입 공부

---
+++ src/variable/Var7.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var7 {
+    static void main(String[] args) {
+        int a = 100; // 정수
+        double b = 3.14; // 실수
+        boolean c = true; // 불리언 true, false
+        char d = 'A'; // 문자 하나, 작은 따옴표 사용
+        String e = "Hello Java"; // 문자열, 큰 따옴표 사용
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
