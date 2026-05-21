4e04037a6995722a3ddfba268a04af89f25066b9
hanjiwon <gomdeschool@naver.com>
2026-05-21 21:18:18 +0900

    5.21 변수 연습 문제 추가

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class VarEx3 {
+    static void main(String[] args) {
+        long num1 = 10000000000L;
+        boolean a = true;
+        System.out.println(num1);
+        System.out.println(a);
+    }
+}
